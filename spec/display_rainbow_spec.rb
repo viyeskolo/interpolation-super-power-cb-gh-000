805ab8f66ee026ed8f0fe0d2c3f1814b4858a4a6
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-24 23:23:24 +0000

    Automatically backed up by Learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -1,3 +1,5 @@
 display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-puts "R: #{red}, O: #{orange}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
+def print_display_rainbow
+  puts "R: #{red}, O: #{orange}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
+end
