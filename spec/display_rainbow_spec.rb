251ef0113d3e6a3cf420061934919384f289390d
Oğuzhan Öztürk <viyeskolo5@gmail.com>
2020-05-24 23:21:50 +0000

    Automatically backed up by Learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -1,7 +1,3 @@
 display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 puts "R: #{red}, O: #{orange}, Y: #{yellow}, G: #{green}, B: #{blue}, I: #{indigo}, V: #{violet}"
-
-
-
-
